a21bc7cee00840957fe35991c9072ad1f20a212a
haklesh051 <hakleshbairwa051@gmail.com>
2025-08-17 00:08:02 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,4 @@
+class Translation:
+    SAVED_CUSTOM_THUMB_NAIL = "✅ Thumbnail saved successfully!"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Thumbnail deleted successfully!"
+    NO_THUMB_FOUND = "⚠️ No thumbnail found!"
