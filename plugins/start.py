79bc99aeae73a69c86a9fd882e7e6a95a3dfde2c
haklesh051 <hakleshbairwa051@gmail.com>
2025-08-17 01:37:59 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,11 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from bot import bot
+
+@bot.on_message(filters.command("start"))
+async def start_handler(client, message: Message):
+    await message.reply_text(
+        f"👋 Hello {message.from_user.first_name}!\n\n"
+        "✅ Bot is alive and running...\n"
+        "Use /help to see available commands."
+    )
