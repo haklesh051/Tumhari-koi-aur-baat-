463d32978b18e5e3dad42326109662721f97bd92
haklesh051 <hakleshbairwa051@gmail.com>
2025-08-17 01:46:50 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from bot import bot
 
-@bot.on_message(filters.command("start"))
+# 👇 Yaha 'client' instance Pyrogram khud pass karega
+@Client.on_message(filters.command("start"))
 async def start_handler(client, message: Message):
     await message.reply_text(
         f"👋 Hello {message.from_user.first_name}!\n\n"
