1d09529122b6b9873630519b5c3f127c8c5351f7
haklesh051 <hakleshbairwa051@gmail.com>
2025-08-17 01:36:02 +0530

    Create thumb.py

---
+++ plugins/thumb.py
@@ -0,0 +1,48 @@
+import os
+from pyrogram import filters, Client
+from translation import Translation
+from database.database import df_thumb, del_thumb, thumb
+from config import Config
+
+@Client.on_message(filters.photo)
+async def save_photo(bot, update):
+    if update.from_user.id in Config.BANNED_USERS:
+        await bot.delete_messages(update.chat.id, update.message_id, revoke=True)
+        return
+
+    download_location = f"{Config.DOWNLOAD_LOCATION}/{update.from_user.id}.jpg"
+    os.makedirs(Config.DOWNLOAD_LOCATION, exist_ok=True)  # ensure folder exists
+    await df_thumb(update.from_user.id, update.message_id)
+    await bot.download_media(update, file_name=download_location)
+    await bot.send_message(update.chat.id, Translation.SAVED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
+
+@Client.on_message(filters.command(["delthumb"]))
+async def delete_thumbnail(bot, update):
+    thumb_image_path = f"{Config.DOWNLOAD_LOCATION}/{update.from_user.id}.jpg"
+    try:
+        await del_thumb(update.from_user.id)
+    except: 
+        pass
+    try:
+        os.remove(thumb_image_path)
+    except: 
+        pass
+    await bot.send_message(update.chat.id, Translation.DEL_ETED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
+
+@Client.on_message(filters.command(["showthumb"]))
+async def show_thumb(bot, update):
+    thumb_image_path = f"{Config.DOWNLOAD_LOCATION}/{update.from_user.id}.jpg"
+    if not os.path.exists(thumb_image_path):
+        mes = await thumb(update.from_user.id)
+        if mes != None:
+            m = await bot.get_messages(update.chat.id, mes.msg_id)
+            await m.download(file_name=thumb_image_path)
+        else:
+            thumb_image_path = None
+    if thumb_image_path is not None and os.path.exists(thumb_image_path):
+        try:
+            await bot.send_photo(update.chat.id, thumb_image_path, reply_to_message_id=update.message_id)
+        except: 
+            pass
+    else:
+        await bot.send_message(update.chat.id, Translation.NO_THUMB_FOUND, reply_to_message_id=update.message_id)
